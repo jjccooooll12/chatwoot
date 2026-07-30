0d88d7fc264f4cf51b24e335ea8282f232d29592
Jason <jjccooooll12@gmail.com>
2026-07-30 08:15:15 +0000

    fix(voice): position the incoming-call widget bottom-left, not bottom-right

--- app/javascript/dashboard/components-next/call/FloatingCallWidget.vue
+++ app/javascript/dashboard/components-next/call/FloatingCallWidget.vue
@@ -238,7 +238,7 @@ onBeforeUnmount(stopRingtone);
 <template>
   <div
     v-if="incomingCalls.length || hasActiveCall"
-    class="fixed ltr:right-4 rtl:left-4 bottom-4 z-50 flex flex-col gap-3 w-[400px]"
+    class="fixed ltr:left-4 rtl:right-4 bottom-4 z-50 flex flex-col gap-3 w-[400px]"
   >
     <!-- Stacked incoming calls (shown above the primary card) -->
     <CallCard
